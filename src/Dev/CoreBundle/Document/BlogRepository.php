a07a729078f572663779c9e47411dfed4f6fc014
Emmanuel Desir <edesir@enablon.net>
2014-03-20 01:33:24 +0100

    avec le text du blog en raw

--- src/Dev/CoreBundle/Document/BlogRepository.php
+++ src/Dev/CoreBundle/Document/BlogRepository.php
@@ -13,10 +13,6 @@ class BlogRepository extends DocumentRepository
 	//retourne les $n derniers blogs
 	public function findLast($n)
     {
-		// $query = $this->dm->createQuery('SELECT b FROM Dev\CoreBundle\Document\Blog b ORDER BY b.created DESC')
-		// 	->setMaxResults($n);
-	 //    return $query->getResult();
-
 	    return $this->createQueryBuilder()
             ->sort('created', 'DESC')
             ->getQuery()
